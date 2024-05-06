1941b965a6613c2cf51a7a9e700d17cdb187a477
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2024-05-06 21:38:32 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
     );
   }
 }
