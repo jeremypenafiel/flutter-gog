e3c345d84ddc7c9a1bec7976534d0bcc0d160f34
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2024-05-26 00:10:32 +0800

    fixed game controller issue

--- lib/ui/BoardScreen/board_screen.dart
+++ lib/ui/BoardScreen/board_screen.dart
@@ -2,12 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gog/backend/game_controller.dart';
+import 'package:gog/main.dart';
 import 'package:gog/ui/BoardScreen/prematch_board_ui.dart';
 import 'package:gog/ui/BoardScreen/board_ui.dart';
 import 'package:gog/ui/BoardScreen/board_screen_bottom_nav_bar.dart';
 import 'package:gog/backend/prematch_board.dart';
 import 'package:gog/ui/BoardScreen/board_screen_buttons.dart';
 import 'package:provider/provider.dart';
+
+import '../../backend/board.dart';
 
 class BoardScreen extends StatefulWidget {
   const BoardScreen({super.key});
@@ -20,8 +23,29 @@ class _BoardScreenState extends State<BoardScreen> {
   final int _selectedIndex = 0;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final gameController = Provider.of<GameController>(context, listen: false);
+    gameController.connect(Board(), PrematchBoard());
+
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    gameController?.board.dispose();
+    gameController?.prematchBoard.dispose();
+
+  }
+
+  @override
   Widget build(BuildContext context) {
     final gameController = Provider.of<GameController>(context, listen: false);
+    if(gameController.prematchBoard == null){
+
+    }
     gameController.startPrematch();
     final prematchBoard = gameController.prematchBoard;
 
