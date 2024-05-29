04f8d822e9bb66c5344893d405714c1d5b9ac6c8
Pan <faferrer@up.edu.ph>
2024-05-29 16:11:31 +0800

    fixed selected tab color at bottomnavbar

--- lib/ui/BoardScreen/board_screen_bottom_nav_bar.dart
+++ lib/ui/BoardScreen/board_screen_bottom_nav_bar.dart
@@ -4,8 +4,14 @@ import 'package:provider/provider.dart';
 import 'scoreboard_ui.dart';
 import 'graveyard_ui.dart';
 
+import 'package:flutter/material.dart';
+import 'package:gog/backend/game_controller.dart';
+import 'package:provider/provider.dart';
+import 'scoreboard_ui.dart';
+import 'graveyard_ui.dart';
+
 class BoardScreenBottomNavBar extends StatefulWidget {
-  const BoardScreenBottomNavBar({Key? key}) : super(key: key);
+  const BoardScreenBottomNavBar({super.key});
 
   @override
   _BoardScreenBottomNavBarState createState() => _BoardScreenBottomNavBarState();
@@ -16,23 +22,25 @@ class _BoardScreenBottomNavBarState extends State<BoardScreenBottomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-  var gameController =  Provider.of<GameController>(context, listen: false);
-  var graveyardWidget= ChangeNotifierProvider.value(value: gameController.board,
-  child: const GraveyardWidget(),);
+    var gameController = Provider.of<GameController>(context, listen: false);
+    var graveyardWidget = ChangeNotifierProvider.value(
+      value: gameController.board,
+      child: const GraveyardWidget(),
+    );
     return Column(
       children: [
         ScoreWidget(),
         Expanded(
           child: _selectedIndex == 0 ? graveyardWidget : HierarchyWidget(),
         ),
-        BottomNavigationBar(
+        CustomBottomNavigationBar(
           items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.sick),
+            CustomBottomNavigationBarItem(
+              icon: Icons.sick,
               label: 'Graveyard',
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.reduce_capacity),
+            CustomBottomNavigationBarItem(
+              icon: Icons.reduce_capacity,
               label: 'Hierarchy',
             ),
           ],
@@ -47,6 +55,64 @@ class _BoardScreenBottomNavBarState extends State<BoardScreenBottomNavBar> {
     );
   }
 }
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final List<CustomBottomNavigationBarItem> items;
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.items,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: items.asMap().entries.map((entry) {
+        int index = entry.key;
+        CustomBottomNavigationBarItem item = entry.value;
+        bool isSelected = index == currentIndex;
+
+        return Expanded(
+          child: GestureDetector(
+            onTap: () => onTap(index),
+            child: ClipRRect(
+              child: Container(
+                color: isSelected ? const Color.fromARGB(255, 38, 38, 38) : Colors.black,
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(item.icon, color: Colors.white),
+                    Text(
+                      item.label,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class CustomBottomNavigationBarItem {
+  final IconData icon;
+  final String label;
+
+  const CustomBottomNavigationBarItem({
+    required this.icon,
+    required this.label,
+  });
+}
+
+
 
 class HierarchyWidget extends StatelessWidget {
   @override
