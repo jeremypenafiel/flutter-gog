cbfed5ec0b6c6cabf6c34af8a4b2c664fdcd4217
Pan <faferrer@up.edu.ph>
2024-05-30 18:55:04 +0800

    added padding

--- lib/ui/BoardScreen/hierarchy_ui.dart
+++ lib/ui/BoardScreen/hierarchy_ui.dart
@@ -3,212 +3,214 @@ import 'package:flutter/material.dart';
 class HierarchyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double spacing = MediaQuery.of(context).size.width * 0.02; // 2% of screen width
+    double padding = 8.0;
+
     return SingleChildScrollView(
-      child: Container (
+      child: Container(
         color: const Color.fromARGB(255, 38, 38, 38),
         child: Center(
-          child: Column(
-            children: [
-              const Text(
-                'Hierarchy',
-                style: TextStyle(color: Colors.white),
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_5star_general.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+          child: Padding(
+            padding: EdgeInsets.all(padding),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_5star_general.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_4star_general.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_4star_general.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_3star_general.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_3star_general.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_2star_general.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_2star_general.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_1star_general.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_1star_general.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_colonel.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_colonel.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_lt_colonel.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_lt_colonel.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_major.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_major.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_captain.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_captain.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_1st_lieut.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_1st_lieut.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_2nd_lieut.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_2nd_lieut.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any lower-ranking officer below it, the Private, and the Flag except the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_sergeant.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates only the Private and the Flag.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_sergeant.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates only the Private and the Flag.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_private.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates only the Spy.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_private.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates only the Spy.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_spy.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Eliminates any piece except the Private.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_spy.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Eliminates any piece except the Private.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/White Pieces/white_flag.png"),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "Can not eliminate any piece. Losing this piece is instant loss and making it reach the other end of the board is instant win.",
-                      style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
-                      overflow: TextOverflow.clip,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/White Pieces/white_flag.png"),
+                    SizedBox(width: spacing),
+                    Expanded(
+                      child: const Text(
+                        "Cannot eliminate any piece. Losing this piece is an instant loss and making it reach the other end of the board is an instant win.",
+                        style: TextStyle(color: Color.fromARGB(255, 240, 238, 238)),
+                        overflow: TextOverflow.clip,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
